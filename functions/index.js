70c1e4e25a42eb4326a15b806aeeebcee0824766
Nico <imiddleton2021@gmail.com>
2025-03-04 16:26:41 -0500

    added firebase functions, got /hello-world working

---
+++ functions/index.js
@@ -0,0 +1,52 @@
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require('cors');
+const app = express();
+
+//Routes
+app.get('/hello-world', (req, res) => {
+    return res.status(200).send('Hello world');
+});
+
+
+//Create
+//Post
+
+//Read
+//Get
+
+//Update
+//Put
+
+//Delete
+//Delete
+
+
+
+//Export the api to Firebase Cloud Functions
+exports.app = functions.https.onRequest(app);
+
+
+
+
+
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
