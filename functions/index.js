de855af3bd08b87bb64cf9417c30174173b08bf0
Nico <imiddleton2021@gmail.com>
2025-03-06 16:00:50 -0500

    added create/post function, tested using postman

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,22 @@ const logger = require("firebase-functions/logger");
 
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+
+var serviceAccount = require("./permissions.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+
+
 const express = require("express");
-const cors = require('cors');
 const app = express();
+const db = admin.firestore();
+
+const cors = require('cors');
+app.use( cors( { origin:true } ) );
 
 //Routes
 app.get('/hello-world', (req, res) => {
@@ -14,6 +27,27 @@ app.get('/hello-world', (req, res) => {
 
 //Create
 //Post
+app.post('/api/create', (req, res) => {
+    
+    (async () => {
+            try{
+                await db.collection('switch').doc('/' + req.body.id + '/')
+                .create({
+                    timestamp: req.body.timestamp,
+                    temp: req.body.temp,
+                    humidity: req.body.humidity,
+                    smoke_level: req.body.smoke_level
+                })
+
+                return res.status(200).send();
+            }
+            catch(error){
+                console.log(error);
+                return res.status(500).send(error);
+            }
+    })();
+});
+
 
 //Read
 //Get
