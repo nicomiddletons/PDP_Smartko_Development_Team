903bcbf7b6235ff86b879819cad94ab451aaf583
Nico <imiddleton2021@gmail.com>
2025-03-06 16:48:23 -0500

    deployed

--- functions/index.js
+++ functions/index.js
@@ -1,119 +1,107 @@
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
-var serviceAccount = require("./permissions.json");
+const serviceAccount = require("./permissions.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
-
-
 
 
 const express = require("express");
 const app = express();
 const db = admin.firestore();
 
-const cors = require('cors');
-app.use( cors( { origin:true } ) );
+const cors = require("cors");
+app.use( cors( {origin: true} ) );
 
-//Routes
-app.get('/hello-world', (req, res) => {
-    return res.status(200).send('Hello world');
+// Routes
+app.get("/hello-world", (req, res) => {
+  return res.status(200).send("Hello world");
 });
 
-//Create
-//Post
-app.post('/api/create', (req, res) => {
-    
-    (async () => {
-            try{
-                await db.collection('switch').doc('/' + req.body.id + '/')
-                .create({
-                    timestamp: req.body.timestamp,
-                    temp: req.body.temp,
-                    humidity: req.body.humidity,
-                    smoke_level: req.body.smoke_level
-                })
+// Create
+// Post
+app.post("/api/create", (req, res) => {
+  (async () => {
+    try {
+      await db.collection("switch").doc("/" + req.body.id + "/")
+          .create({
+            timestamp: req.body.timestamp,
+            temp: req.body.temp,
+            humidity: req.body.humidity,
+            smoke_level: req.body.smoke_level,
+          });
 
-                return res.status(200).send();
-            }
-            catch(error){
-                console.log(error);
-                return res.status(500).send(error);
-            }
-    })();
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 });
 
 
-//Read a specific timestamp based on ID
-//Get
-app.get('/api/read/:id', (req, res) => {
-    
-    (async () => {
-            try{
-                const document = db.collection('switch').doc(req.params.id);
-                let switchData = await document.get();
-                let response = switchData.data();
+// Read a specific timestamp based on ID
+// Get
+app.get("/api/read/:id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("switch").doc(req.params.id);
+      const switchData = await document.get();
+      const response = switchData.data();
 
-                return res.status(200).send(response);
-            }
-            catch(error){
-                console.log(error);
-                return res.status(500).send(error);
-            }
-    })();
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 });
 
-//Read all timestamps
-//Get
-app.get('/api/read', (req, res) => {
-    
-    (async () => {
-            try{
-                let query = db.collection('switch');
-                let response = [];
+// Read all timestamps
+// Get
+app.get("/api/read", (req, res) => {
+  (async () => {
+    try {
+      const query = db.collection("switch");
+      const response = [];
 
-                await query.get().then(querySnapshot => {
-                    let docs = querySnapshot.docs; //the result of the query
+      await query.get().then((querySnapshot) => {
+        const docs = querySnapshot.docs; // the result of the query
 
-                    for(let doc of docs){
-                        const selectedItem = {
-                            id: doc.id,
-                            timestamp: doc.data().timestamp,
-                            temp: doc.data().temp,
-                            humidity: doc.data().humidity,
-                            smoke_level: doc.data().smoke_level
-                        };
-                        response.push(selectedItem);
-                    }
-                    return response; //each then should return a value
-                })
-                return res.status(200).send(response);
-            }
-            catch(error){
-                console.log(error);
-                return res.status(500).send(error);
-            }
-    })();
+        for (const doc of docs) {
+          const selectedItem = {
+            id: doc.id,
+            timestamp: doc.data().timestamp,
+            temp: doc.data().temp,
+            humidity: doc.data().humidity,
+            smoke_level: doc.data().smoke_level,
+          };
+          response.push(selectedItem);
+        }
+        return response; // each then should return a value
+      });
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 });
 
-//Update
-//Put
+// Update
+// Put
 
-//Delete
-//Delete
+// Delete
+// Delete
 
 
-
-//Export the api to Firebase Cloud Functions
+// Export the api to Firebase Cloud Functions
 exports.app = functions.https.onRequest(app);
-
-
-
 
 
 /**
