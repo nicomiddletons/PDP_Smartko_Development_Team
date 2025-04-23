cca299562cf79e97dc4f55984f3f4274ef68ba13
Ttan-24 <tanviprakash24@gmail.com>
2025-04-23 14:42:05 -0400

    Yearly graph display for the voltage phase 1, 2, 3, current phase 1, 2, 3 and total power consumption.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -46,6 +46,9 @@ function App() {
   const [chartYearlyTempData, setChartYearlyTempData] = useState({});
   const [chartYearlyHumidityData, setChartYearlyHumidityData] = useState({});
   const [chartYearlySmokeData, setChartYearlySmokeData] = useState({});
+  const [chartYearlyVoltage1Data, setChartYearlyVoltage1Data] = useState({});
+  const [chartYearlyCurrent1Data, setChartYearlyCurrent1Data] = useState({});
+  const [chartYearlyTotalPowerData, setChartYearlyTotalPowerData] = useState({});
 
   useEffect(() => {
     axios
@@ -180,13 +183,76 @@ function App() {
           yearlyHumidityData[monthIndex].push(item.humidity);
         });
 
-        // Group smoke level data by month
+        // Group smoke level yearly data
         const yearlySmokeData = {};
         yearlyData.forEach((item) => {
           const monthIndex = new Date(item.timestamp).getMonth(); // Get month index (0-11)
 
           if (!yearlySmokeData[monthIndex]) yearlySmokeData[monthIndex] = [];
           yearlySmokeData[monthIndex].push(item.smoke_level);
+        });
+
+        // Group voltage phase 1 yearly data
+        const yearlyVoltage1Data = {};
+        yearlyData.forEach((item) => {
+          const monthIndex = new Date(item.timestamp).getMonth(); // Get month index (0-11)
+
+          if (!yearlyVoltage1Data[monthIndex]) yearlyVoltage1Data[monthIndex] = [];
+          yearlyVoltage1Data[monthIndex].push(item.voltage_phase_1);
+        });
+
+        // Group voltage phase 2 yearly data
+        const yearlyVoltage2Data = {};
+        yearlyData.forEach((item) => {
+          const monthIndex = new Date(item.timestamp).getMonth(); // Get month index (0-11)
+
+          if (!yearlyVoltage2Data[monthIndex]) yearlyVoltage2Data[monthIndex] = [];
+          yearlyVoltage2Data[monthIndex].push(item.voltage_phase_2);
+        });
+
+        // Group voltage phase 3 yearly data
+        const yearlyVoltage3Data = {};
+        yearlyData.forEach((item) => {
+          const monthIndex = new Date(item.timestamp).getMonth(); // Get month index (0-11)
+
+          if (!yearlyVoltage3Data[monthIndex]) yearlyVoltage3Data[monthIndex] = [];
+          yearlyVoltage3Data[monthIndex].push(item.voltage_phase_3);
+        });
+
+        // Group current phase 1 yearly data
+        const yearlyCurrent1Data = {};
+        yearlyData.forEach((item) => {
+          const monthIndex = new Date(item.timestamp).getMonth(); // Get month index (0-11)
+
+          if (!yearlyCurrent1Data[monthIndex]) yearlyCurrent1Data[monthIndex] = [];
+          yearlyCurrent1Data[monthIndex].push(item.current_phase_1);
+        });
+
+        // Group current phase 2 yearly data
+        const yearlyCurrent2Data = {};
+        yearlyData.forEach((item) => {
+          const monthIndex = new Date(item.timestamp).getMonth(); // Get month index (0-11)
+
+          if (!yearlyCurrent2Data[monthIndex]) yearlyCurrent2Data[monthIndex] = [];
+          yearlyCurrent2Data[monthIndex].push(item.current_phase_2);
+        });
+
+        // Group current phase 3 yearly data
+        const yearlyCurrent3Data = {};
+        yearlyData.forEach((item) => {
+          const monthIndex = new Date(item.timestamp).getMonth(); // Get month index (0-11)
+
+          if (!yearlyCurrent3Data[monthIndex]) yearlyCurrent3Data[monthIndex] = [];
+          yearlyCurrent3Data[monthIndex].push(item.current_phase_3);
+        });
+
+        // Group Total Power yearly data
+        const yearlyTotalPowerData = {};
+        yearlyData.forEach((item) => {
+          const monthIndex = new Date(item.timestamp).getMonth(); // Get month index (0-11)
+
+          if (!yearlyTotalPowerData[monthIndex]) yearlyTotalPowerData[monthIndex] = [];
+          yearlyTotalPowerData[monthIndex].push(item.total_power);
         });
 
         // Monthly
@@ -300,6 +366,54 @@ function App() {
             : null; // Avg smoke level per month (null for missing months)
         });
 
+        const yearlyVoltage1Points = monthLabels.map((_, index) => {
+          const voltages1 = yearlyVoltage1Data[index] || [];
+          return voltages1.length > 0
+            ? voltages1.reduce((sum, voltage1) => sum + voltage1, 0) / voltages1.length
+            : null; // Avg voltage 1 per month (null for missing months)
+        });
+
+        const yearlyVoltage2Points = monthLabels.map((_, index) => {
+          const voltages2 = yearlyVoltage2Data[index] || [];
+          return voltages2.length > 0
+            ? voltages2.reduce((sum, voltage2) => sum + voltage2, 0) / voltages2.length
+            : null; // Avg voltage 2 per month (null for missing months)
+        });
+
+        const yearlyVoltage3Points = monthLabels.map((_, index) => {
+          const voltages3 = yearlyVoltage3Data[index] || [];
+          return voltages3.length > 0
+            ? voltages3.reduce((sum, voltage3) => sum + voltage3, 0) / voltages3.length
+            : null; // Avg voltage 3 per month (null for missing months)
+        });
+
+        const yearlyCurrent1Points = monthLabels.map((_, index) => {
+          const currents1 = yearlyCurrent1Data[index] || [];
+          return currents1.length > 0
+            ? currents1.reduce((sum, current1) => sum + current1, 0) / currents1.length
+            : null; // Avg current 1 per month (null for missing months)
+        });
+
+        const yearlyCurrent2Points = monthLabels.map((_, index) => {
+          const currents2 = yearlyCurrent2Data[index] || [];
+          return currents2.length > 0
+            ? currents2.reduce((sum, current2) => sum + current2, 0) / currents2.length
+            : null; // Avg current 1 per month (null for missing months)
+        });
+
+        const yearlyCurrent3Points = monthLabels.map((_, index) => {
+          const currents3 = yearlyCurrent3Data[index] || [];
+          return currents3.length > 0
+            ? currents3.reduce((sum, current3) => sum + current3, 0) / currents3.length
+            : null; // Avg current 1 per month (null for missing months)
+        });
+
+        const yearlyTotalPowerPoints = monthLabels.map((_, index) => {
+          const totalpowers = yearlyTotalPowerData[index] || [];
+          return totalpowers.length > 0
+            ? totalpowers.reduce((sum, totalpower) => sum + totalpower, 0) / totalpowers.length
+            : null; // Avg current 1 per month (null for missing months)
+        });
         // Update temp
         setChartTempData({
           labels,
@@ -504,6 +618,75 @@ function App() {
               data: yearlySmokePoints,
               borderColor: "#ECDBE9",
               backgroundColor: "rgba(236, 219, 233, 1)",
+              tension: 0.4,
+            },
+          ],
+        });
+
+        // Update state with chart data
+        setChartYearlyVoltage1Data({
+          labels: monthLabels,
+          datasets: [
+            {
+              label: "Voltage Phase 1 - Yearly",
+              data: yearlyVoltage1Points,
+              borderColor: "#FF5733",
+              backgroundColor: "rgba(255, 87, 51, 0.2)",
+              tension: 0.4,
+            },
+            {
+              label: "Voltage Phase 2 - Yearly",
+              data: yearlyVoltage2Points,
+              borderColor: "#FF5733",
+              backgroundColor: "rgba(255, 87, 51, 0.2)",
+              tension: 0.4,
+            },
+            {
+              label: "Voltage Phase 3 - Yearly",
+              data: yearlyVoltage3Points,
+              borderColor: "#FF5733",
+              backgroundColor: "rgba(255, 87, 51, 0.2)",
+              tension: 0.4,
+            },
+          ],
+        });
+
+        // Update state with chart data
+        setChartYearlyCurrent1Data({
+          labels: monthLabels,
+          datasets: [
+            {
+              label: "Current Phase 1 - Yearly",
+              data: yearlyCurrent1Points,
+              borderColor: "#FF5733",
+              backgroundColor: "rgba(255, 87, 51, 0.2)",
+              tension: 0.4,
+            },
+            {
+              label: "Current Phase 2 - Yearly",
+              data: yearlyCurrent2Points,
+              borderColor: "#FF5733",
+              backgroundColor: "rgba(255, 87, 51, 0.2)",
+              tension: 0.4,
+            },
+            {
+              label: "Current Phase 3 - Yearly",
+              data: yearlyCurrent3Points,
+              borderColor: "#FF5733",
+              backgroundColor: "rgba(255, 87, 51, 0.2)",
+              tension: 0.4,
+            },
+          ],
+        });
+
+        setChartYearlyTotalPowerData({
+          labels: monthLabels,
+          datasets: [
+            {
+              label: "Total Power Current Year",
+              data: yearlyTotalPowerPoints,
+              borderColor: "#4CAF50",
+              backgroundColor: "rgba(76, 175, 80, 0.2)",
               tension: 0.4,
             },
           ],
@@ -1146,6 +1329,24 @@ function App() {
                   <h4 className="chartcard-h4">Smoke Level Trend (Yearly)</h4>
                   <div className="chartcard smoke_level">
                     <Line data={chartYearlySmokeData} />
+                  </div>
+                </div>
+                <div className="chartcard">
+                  <h4 className="chartcard-h4">Voltage Phase 1, 2, 3 Trend (Yearly)</h4>
+                  <div className="chartcard voltagePhase">
+                    <Line data={chartYearlyVoltage1Data} />
+                  </div>
+                </div>
+                <div className="chartcard">
+                  <h4 className="chartcard-h4">Current Phase 1, 2, 3 Trend (Yearly)</h4>
+                  <div className="chartcard currentPhase">
+                    <Line data={chartYearlyCurrent1Data} />
+                  </div>
+                </div>
+                <div className="chartcard">
+                  <h4 className="chartcard-h4">Total Power Trend (Yearly)</h4>
+                  <div className="chartcard totalPowerPhase">
+                    <Line data={chartYearlyTotalPowerData} />
                   </div>
                 </div>
               </div>
